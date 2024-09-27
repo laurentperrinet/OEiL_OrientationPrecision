e328aac8bff50b594be0bbe308b19f936e707d24
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2024-09-27 20:34:09 +0200

    Update write_data.php

--- write_data.php
+++ write_data.php
@@ -1,7 +1,7 @@
 <?php
 // get the data from the POST message
 $post_data = json_decode(file_get_contents('php://input'), true);
-$data = $post_data['data'];
+$data = $post_data['filedata'];
 // generate a unique ID for the file, e.g., session-6feu833950202 
 $file = uniqid("session-");
 // the directory "data" must be writable by the server
