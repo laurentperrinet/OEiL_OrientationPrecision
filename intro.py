7938879e924c92c6ae809901aac9a3cd752a2b05
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2024-11-26 13:10:06 +0100

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,23 +1,30 @@
 from manim import *
 
+
+import MotionClouds as mc
+
+
 class Intro(Scene):
     def construct(self):
+
+
+
+        # my_object = MathTex(r"\Updownarrow").scale(8)
+        my_object = Rectangle(width=1.0, height=4.0)
+        # square = Square()  # create a square
+        run_time = 3
+        self.play(Create(my_object), run_time=run_time)  # show the square on screen
         self.play(
-            Rotate(
-                MathTex(r"\bot").scale(8),
-                angle=20/180*PI,
-                about_point=ORIGIN,
-                rate_func=linear,
-            ),
-        )
-        self.play(
-            Rotate(
-                MathTex(r"\bot").scale(8),
-                angle=-20/180*PI,
-                about_point=ORIGIN,
-                rate_func=linear,
-            ),
-        )
+            my_object.animate.set_fill(PINK, opacity=0.5)
+        )  # color the text on screen
+        self.play(my_object.animate.rotate(PI / 5), run_time=run_time/2)  # rotate the square
+        self.play(my_object.animate.rotate(-PI / 2.5), run_time=run_time)  # rotate the square
+
+        vertical = DashedLine(config.top, config.bottom, dash_length=2.0, color=RED)
+
+        self.add(vertical)  # show the square on screen
         
 
+        self.play(my_object.animate.rotate(PI / 2.5), run_time=run_time)  # rotate the square
+        self.play(my_object.animate.rotate(-PI / 2.5), run_time=run_time)  # rotate the square
         
